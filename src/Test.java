0405c4d111df5004b88e6181263542ffd67cbbd1
Thaistranf <anhnamduong9x9@gmail.com>
2023-08-15 11:23:59 +0700

    add

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Thái");
+        student.setClasses("C06");
+
+    }
+}
